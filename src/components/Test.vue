c49cf4b38a7842b0a452c56f8a38170f76d0ff3f
OuYangResume <15180191339@139.com>
2018-03-29 17:22:21 +0800

    提交vue cli手脚架

---
+++ src/components/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="test">
+      <h1> {{title}}</h1>
+      <p>{{user.firstName}}</p>
+      <p v-text="user.lastName"></p>
+  </div>
+</template>
+<script>
+    export default{
+        name:"test",
+        data(){
+            return{
+               title:"hello vue" ,
+               user:{
+                   firstName:"ouyang",
+                   lastName:"xiaogao"
+               }
+            }
+        }
+    }
+</script>
+<style scoped>
+
+</style>
+
