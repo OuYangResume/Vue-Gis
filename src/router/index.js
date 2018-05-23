ab21fa80a9d4cb6f13372c94bf62b3ac676e806c
OuYangResume <15180191339@139.com>
2018-05-23 21:14:55 +0800

    编写axios页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Resource from 'vue-resource'
 import echarts from 'echarts'
-
-Vue.use(Resource)
 
 Vue.prototype.$echarts = echarts
 Vue.use(VueRouter)
@@ -13,14 +10,6 @@ Vue.use(VueRouter)
 import Layout from '../views/layout/Layout'
 
 export const router1=[
-  {
-      path: '/', 
-      component: () => import('@/views/test/user'),
-      //別名。路徑和"/"效果一樣
-      alias: '/user',
-      //重定向。訪問'/'和'/user'到地圖
-      //redirect: '/map'
-    },
     {
       path: "/test", 
       component: () =>import('@/views/test/test')
@@ -68,10 +57,10 @@ const router2=[
         meta: { title: 'test', icon: 'example' },
       },
       {
-        path: 'user',
-        name:'User',
-        component: ()=>import("@/views/test/user"),
-        meta: { title: 'User', icon: 'example' },
+        path: 'axios',
+        name:'Axios',
+        component: ()=>import("@/views/test/axios"),
+        meta: { title: 'Axios', icon: 'example' },
       }
     ]
   },
---
+++ src/views/test/Axios.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <p> 初次遇见 {{name}}</p>
+
+        <!-- 添加用户信息 -->
+      <form v-on:submit="addUser">
+          <input type="text" v-model="newUser.name" placeholder="Enter name">
+          <input type="text" v-model="newUser.email" placeholder="Enter eamil">
+          <input type="submit" value="Submit">
+      </form>
+        <!-- 展示用户信息 -->
+        <ul>
+            <li v-for="user in users ">
+              <input type="checkbox" class="toggle" v-model="user.contacted">
+              <span :class="{contacted:user.contacted}">
+                    {{user.name}}:{{user.email}}:{{user.phone}}
+              <button v-on:click="deleteUser(user)">X</button>
+              </span>
+              </li>
+        </ul>
+    </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      users: [],
+      name: "axios",
+      posts2: null,
+      posts1: null,
+      newUser:{},
+    };
+  },
+  methods: {
+    addUser:function(e){
+          this.users.push({
+              name:this.newUser.name,
+              email:this.newUser.email,
+              contacted:false
+          });
+          e.preventDefault();
+      },
+      deleteUser:function(user){
+          this.users.splice(this.users.indexOf(user),1);
+      }
+  },
+  // 实例创建之后调用，一般获取数据
+  created: function() {
+    // 获取一个请求的数据
+    axios
+      .get("http://jsonplaceholder.typicode.com/posts/1")
+      .then(response => {
+        this.post1 = response.data;
+      })
+      .catch(error => {
+        console.log("axios==" + error);
+      });
+    //同时获取多个请求的数据
+    function getUserData() {
+      return axios.get("http://jsonplaceholder.typicode.com/users");
+    }
+    function getPostData() {
+      return axios.get("http://jsonplaceholder.typicode.com/posts/2");
+    }
+    axios.all([getPostData(), getUserData()]).then(
+      axios.spread((post, user) => {
+        //当这两个请求都完成的时候会触发这个函数，两个参数分别代表返回的结果
+        // console.log(post,user);
+        this.users = user.data;
+        console.log(this.users);
+      })
+    );
+  },
+
+  mounted() {}
+};
+</script>
+
+<style scoped>
+    .contacted{
+        text-decoration: line-through;
+    }
+</style>
+
