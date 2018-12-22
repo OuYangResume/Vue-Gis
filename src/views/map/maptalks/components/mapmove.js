9c45c717f466e0e8604cde4589b82d9f09c8ce22
OuYangResume <15180191339@139.com>
2018-12-22 20:58:27 +0800

    routermove

--- src/views/map/maptalks/components/mapmove.js
+++ src/views/map/maptalks/components/mapmove.js
@@ -1,26 +1,35 @@
 import * as maptalks from 'maptalks';
 
 class realTimeTrance {
-    pointFeature =true;
-    lineFeature =true;
+
     constructor(options) {
-        this.geomline =null
         this._map = options.map;
         this._LinePoints = [];
+        this.pointFeature = true;
+        this.lineFeature = true;
+        this.markerSymbol = {
+            'markerType': 'ellipse',
+            'markerFill': 'rgb(135,196,240)',
+            'markerFillOpacity': 1,
+            'markerLineColor': '#34495e',
+            'markerLineWidth': 3,
+            'markerLineOpacity': 1,
+            'markerLineDasharray': [],
+            'markerWidth': 20,
+            'markerHeight': 20,
+            'markerDx': 0,
+            'markerDy': 0,
+            'markerOpacity': 1
+        }
         this.init();
     }
     init() {
         this._createLayer();
     }
+    /**
+     * 创建_vecLayer图层
+     */
     _createLayer() {
-        // this._vecSource = new ol.source.Vector({
-        //     features: this._features,
-        //     wrapX: false
-        // });
-        // this._vecLayer = new ol.layer.Vector({
-        //     style: this.drawedStyle,
-        //     source: this._vecSource
-        // });
         this._vecLayer = new maptalks.VectorLayer("vector");
         if (!this._map) {
             console.log("未定义map容器")
@@ -28,52 +37,79 @@ class realTimeTrance {
         }
         this._map.addLayer(this._vecLayer);
     }
+    /**
+     * 向_vecLayer图层添加geometry
+     * @param {*} geometry 
+     */
     addGeometry(geometry) {
-        console.log("sad")
         this._vecLayer.addGeometry(geometry);
     }
-    addPoint(point){
+
+    addPoint(point) {
         this._LinePoints.push(point);
-        if(this._LinePoints.length>=2){
-            this._createLine();
+        if (this._LinePoints.length >= 2) {
+            this._createLine(this._LinePoints);
         }
         this._createMarker(point);
-
     }
-
-    _createLine() {
-        if(this.lineFeature){
-            this.geomline = new maptalks.LineString(this._LinePoints, {
+    /**
+     *  创建动态线
+     * @param {*} line 
+     */
+    _createLine(line) {
+        if (this.lineFeature) {
+            this.geomline = new maptalks.LineString(line, {
                 id: "geomline"
-              });
-              var symbol = {
+            });
+            var symbol = {
                 lineColor: "rgba(250,0,0,1)",
-                lineWidth: 8,
+                lineWidth: 4,
                 lineJoin: "round", //miter, round, bevel
                 lineCap: "round", //butt, round, square
                 lineDasharray: null, //dasharray, e.g. [10, 5, 5]
                 "lineOpacity ": 1
-              };
-              this.geomline.setSymbol(symbol);
-              this.addGeometry(this.geomline);
-              this.lineFeature =false;
-              return;
-        }else{
-            this.geomline.setCoordinates(this._LinePoints);
-        }
-        
-      }
-    _createMarker(point){
-        if (this.pointFeature) {
-             this.geompoint =new maptalks.Marker(point,{
-                id:"geompoint"
-            })
-            this._vecLayer.addGeometry(this.geompoint);
-            this.pointFeature =false;
+            };
+            this.geomline.setSymbol(symbol);
+            this.addGeometry(this.geomline);
+            this.lineFeature = false;
             return;
-          } else {
+        } else {
+            this.geomline.setCoordinates(line);
+        }
+
+    }
+    /**
+     *  创建动态点
+     * @param {Array} point 
+     */
+    _createMarker(point) {
+        if (this.pointFeature) {
+            this.geompoint = new maptalks.Marker(point, {
+                id: "geompoint"
+            })
+            this.geompoint.setSymbol(this.markerSymbol)
+            this.addGeometry(this.geompoint);
+            this.pointFeature = false;
+            return;
+        } else {
             this.geompoint.setCoordinates(point);
-          }
+        }
+    }
+    /**
+     *  清除_vecLayer中的geometry和缓存点
+     */
+    clear() {
+        this._vecLayer.clear();
+        this._LinePoints = [];
+        this.lineFeature = true;
+        this.pointFeature = true;
+    }
+    /**
+     * 设置marker的样式
+     * @param {*} style 
+     */
+    setMarkerStyle(style) {
+        this.markerSymbol = style
     }
     test() {
         console.log("测试类里面的方法")
@@ -87,19 +123,16 @@ class realTimeTrance {
 
 
 class Path_Animation {
-    options = {
-        map: null,
-        time: 100
-    };
     /**
      * Path_Animation的构造函数
      * @param {*} options 
      */
     constructor(options) {
-        this.setOptions(this, options);
-        this._map = this.options.map;
-        this.time = this.options.time;
-        this.linePoints = null;
+        this._map = options.map;
+        this.time = 100 || options.time;
+        this.lineData = null || options.lineData; //传入的轨迹数据
+        this.linePoints = null;//生成的新point
+        this.setInterval = null;
         this.isOpen = false;
         this.speed = 2;
         this.init();
@@ -117,43 +150,83 @@ class Path_Animation {
         this.real = new realTimeTrance(option);
     }
     close() {
-        this.isOpen=false;
+        this.isOpen = false;
         if (this.setInterval) {
-          window.clearInterval(this.setInterval);
+            window.clearInterval(this.setInterval);
         }
-        this.clear();
+        this.real.clear();
         this.index = 0;
         this.linePoints = [];
-      }
+    }
+    /**
+     * 开启回放
+     */
     open() {
-        //this.real.test();
-        //this.close();
+        this.close();
         this.isOpen = true;
-        this.linePoints = this.getNewData(this.options.lineData);
-        this.addLine(this.options.lineData);
+        this.addLine(this.lineData);
+        this.linePoints = this.getNewData(this.lineData);
         this.createInterval();
     }
-    createInterval(){
-        var self=this;
-        self.setInterval=setInterval(function(){
-            if(self.isOpen){
-                setTimeout(function(){
-                    if(self.index<self.linePoints.length){
+    /**
+     * 暂停
+     */
+    suspended() {
+        this.isOpen = false;
+    }
+    /**
+     * 继续
+     */
+    continued() {
+        this.isOpen = true;
+    }
+    /**
+     * 设置轨迹数据
+     * @param {*} lineData 
+     */
+    setData(lineData) {
+        this.lineData = lineData;
+        this.linePoints = this.getNewData(lineData);
+    }
+    /**
+     * 设置轨迹播放速度
+     * @param {number} speed 
+     */
+    setSpeed(speed) {
+        this.speed = speed;
+    }
+    /**
+     * 设置动态点的样式
+     * @param {*} style 
+     */
+    setMarkerStyle(style) {
+        this.real.setMarkerStyle(style);
+    }
+    /**
+     * 创建定时器
+     */
+    createInterval() {
+        var self = this;
+        if (self.linePoints == undefined) {
+            console.log("新生成的数据有误")
+            return;
+        }
+        self.setInterval = setInterval(function () {
+            if (self.isOpen) {
+                setTimeout(function () {
+                    if (self.index < self.linePoints.length) {
                         self.real.addPoint(self.linePoints[self.index]);
                         // if(self.index>1){
                         //     var Angle=self.getAngle(self.linePoints[self.index-1],self.linePoints[self.index]);
                         //     self.IconRotation(Angle);
                         // }
                         self.index++;
-                    }else{
-                      //  self.realTimeTrance.clear();
-                        window.clearInterval(self.setInterval);
-                        self.index=0;
-                        self.isOpen=false;
+                    } else {
+                        self.close()
                     }
-                },parseInt(self.time/self.speed))
+                }, parseInt(self.time / self.speed))
             }
-        },parseInt(self.time/self.speed));
+        }, parseInt(self.time / self.speed));
     }
 
     addLine(lineData) {
@@ -170,20 +243,26 @@ class Path_Animation {
         this.real.addGeometry(line);
     }
     getNewData(linePoints) {
-        var points = [];
-        var line = linePoints;
-        //修改为for  of
-        for (var i = 0; i < line.length; i++) {
-            if (i + 1 < line.length) {
-                var lonlats = this.insertPoint(line[i], line[i + 1], this.time);
-                points = points.concat(lonlats);
+        if (linePoints === null || linePoints.length < 2) {
+            console.log("传入的数据格式不符！")
+            return;
+        } else {
+            var points = [];
+            var line = linePoints;
+            //修改为for  of
+            for (var i = 0; i < line.length; i++) {
+                if (i + 1 < line.length) {
+                    var lonlats = this.insertPoint(line[i], line[i + 1], this.time);
+                    points = points.concat(lonlats);
+                }
             }
+            return points;
         }
-        return points;
+
     }
 
     insertPoint(startPoint, endPoint, speed) {
-       // var distance = this.getDistance(startPoint, endPoint, true);
+        // var distance = this.getDistance(startPoint, endPoint, true);
         //var insertPointLength = Math.ceil(distance / (speed));
         var insertPointLength = speed;
         //计算每一段的长度。
--- src/views/map/maptalks/mapmove1.vue
+++ src/views/map/maptalks/mapmove1.vue
@@ -2,16 +2,19 @@
   <div>
     <div id="map"></div>
     <div v-on:click="open">开始</div>
+    <div @click="suspended">暂停</div>
+    <div @click="continued">继续</div>
   </div>
 </template>
 <script>
-import * as maptalks from "maptalks"
-import Path_Animation from "./components/mapmove.js";
+import * as maptalks from "maptalks";
+//import Path_Animation from "./components/mapmove.js";
+import Path_Animation from 'routermove'
 export default {
   data() {
     return {
       map: null,
-      path:null,
+      path: null
     };
   },
   mounted() {
@@ -31,21 +34,55 @@ export default {
             '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>'
         })
       });
+
+      let vecLayer = new maptalks.VectorLayer("vector1").addTo(vm.map);
+      let point = [111.32450763502036, 31.667512417065313];
+      let geompoint = new maptalks.Marker(point, {
+        id: "geompoint1"
+      });
+      let style = {
+        markerFile: "../../../assets/images/mouseStyle.png",
+        markerWidth: 29,
+        markerHeight: 29,
+        markerDy: -20
+      };
+      geompoint.setSymbol(style);
+      vecLayer.addGeometry(geompoint);
       let option = {
-        name: "oouyang",
         map: vm.map,
         lineData: [
+          [111.32450763502036, 31.667512417065313],
+          [113.32450763502036, 33.667512417065313],
+          [114.32450763502036, 31.667512417065313],
+          [114.32450763502036, 36.667512417065313],
+          [116.32450763502036, 35.667512417065313]
+        ]
+      };
+      vm.path = new Path_Animation(option);
+    },
+    open() {
+      let lineData = [
         [111.32450763502036, 31.667512417065313],
         [113.32450763502036, 33.667512417065313],
-        [114.32450763502036, 31.667512417065313],
         [114.32450763502036, 36.667512417065313],
         [116.32450763502036, 35.667512417065313]
-      ],
+      ];
+      //this.path.setData(lineData);
+      this.path.setSpeed(4);
+      let style = {
+        markerFile: "../../../../static/images/mouseStyle.png",
+        markerWidth: 29,
+        markerHeight: 29,
+        markerDy: -20
       };
-       vm.path = new Path_Animation(option);
+      //this.path.setMarkerStyle(style);
+      this.path.open();
     },
-    open(){
-        this.path.open();
+    suspended() {
+      this.path.suspended();
+    },
+    continued() {
+      this.path.continued();
     }
   }
 };
