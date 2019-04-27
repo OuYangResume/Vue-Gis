5a961e954205ac7525f7e748f7ed6daf3786fe01
root <812634676@qq.com>
2019-04-27 22:25:49 +0800

    a

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -3,6 +3,7 @@ import VeeValidate from "vee-validate";
 
 import VueI18n from "vue-i18n";
 import zh_CN from "vee-validate/dist/locale/zh_CN";
+import { Validator } from "vee-validate";
 
 Vue.use(VueI18n);
 const i18n = new VueI18n({
@@ -15,3 +16,33 @@ Vue.use(VeeValidate, {
     zh_CN
   }
 });
+
+/*自定义方法*/
+
+Validator.extend("mobile", {
+  getMessage: field => "电话号码格式不正确",
+  validate: value =>
+      value.length === 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/.test(value)
+});
+
+Validator.extend("chinese", {
+  getMessage: field => "只能填写中文",
+  validate: value =>
+      /[^\u0000-\u00FF]/.test(value)
+});
+
+/*自定义错误消息*/
+const Dictionary = {
+  zh_CN: {
+      messages: {
+          required: (field) => field+"是必填项。",
+      },
+      attributes: {
+          email: "邮箱",
+          mobile: '电话号码',
+          idCard: "身份证号码",
+      }
+  }
+};
+// 自定义validate error 信息
+Validator.localize(Dictionary);
--- src/views/test/VeeValidate.vue
+++ src/views/test/VeeValidate.vue
@@ -1,8 +1,75 @@
 <template>
-  <div class="fromdata">
-    <div>
-      <input name="name" type="text" v-model="name" placeholder="姓名" v-validate="'required'">
-      <span v-show="errors.has('name')" class="help">{{ errors.first('name') }}</span>
+  <div>
+    <div class="fromdata">
+      <div class="fromItem">
+        <input
+          name="name"
+          type="text"
+          v-model="fromData.name"
+          placeholder=" 请输入姓名"
+          v-validate="'required|chinese'"
+          data-vv-as="姓名"
+        >
+        <span v-show="errors.has('name')" class="help">{{ errors.first('name') }}</span>
+      </div>
+      <div class="fromItem">
+        <input
+          name="age"
+          type="text"
+          v-model="fromData.age"
+          placeholder="年龄"
+          v-validate="'required'"
+        >
+        <span v-show="errors.has('age')" class="help">{{ errors.first('age') }}</span>
+      </div>
+      <div class="fromItem">
+        <input
+          name="tel"
+          type="text"
+          v-model="fromData.tel"
+          placeholder="电话号码"
+          v-validate="'mobile'"
+        >
+        <span v-show="errors.has('tel')" class="help">{{ errors.first('tel') }}</span>
+      </div>
+      <button @click="commitData">提交</button>
+    </div>
+    <div class="fromdata">
+      <div class="fromItem">
+        <input
+          name="name1"
+          type="text"
+          v-model="fromData1.name"
+          placeholder=" 请输入姓名"
+          v-validate="'required'"
+          data-vv-as="姓名"
+          data-vv-scope="form_1"
+        >
+        <span v-show="errors.has('form_1.name1')" class="help">{{ errors.first('form_1.name1') }}</span>
+      </div>
+      <div class="fromItem">
+        <input
+          name="age1"
+          type="text"
+          v-model="fromData1.age"
+          placeholder="年龄"
+          v-validate="'required'"
+          data-vv-scope="form_2"
+        >
+        <span v-show="errors.has('form_2.age1')" class="help">{{ errors.first('form_2.age1') }}</span>
+      </div>
+      <div class="fromItem">
+        <input
+          name="tel1"
+          type="text"
+          v-model="fromData1.tel"
+          placeholder="电话号码"
+          v-validate="'mobile'"
+          data-vv-scope="form_1"
+        >
+        <span v-show="errors.has('form_1.tel1')" class="help">{{ errors.first('form_1.tel1') }}</span>
+      </div>
+      <button @click="commitData1">提交</button>
     </div>
   </div>
 </template>
@@ -11,8 +78,39 @@
 export default {
   data() {
     return {
-      name: ""
+      fromData: {
+        name: "",
+        age: "",
+        tel: ""
+      },
+      fromData1: {
+        name: "",
+        age: "",
+        tel: ""
+      }
     };
+  },
+  methods: {
+    commitData() {
+      this.$validator.validateAll().then(result => {
+        if (result) {
+          
+          alert("验证通过");
+          return;
+        }
+        alert("请检查表单");
+      });
+    },
+    commitData1() {
+      this.$validator.validate("form_1.*").then(result => {
+        if (result) {
+          
+          alert("验证通过");
+          return;
+        }
+        alert("请检查表单");
+      });
+    }
   }
 };
 </script>
@@ -20,6 +118,16 @@ export default {
 <style lang="scss" scoped>
 .fromdata {
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  .fromItem {
+    display: flex;
+    flex-direction: row;
+    input {
+      height: 30px;
+      width: 200px;
+    }
+  }
 }
 </style>
 
